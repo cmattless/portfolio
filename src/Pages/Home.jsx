cdf435fa094701f6be6523b576f2ed822f79fa90
ConnaaaR69 <connormattless@gmail.com>
2023-11-16 16:11:21 +0000

    extended first tile on homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -39,6 +39,12 @@ const Home = () => {
 						<h2 className="text-primary-content text-xl sm:text-2xl md:text-3xl mt-4">
 							I build engaging & accessible digital experiences.
 						</h2>
+						<h3 className="text-primary-content">
+							Want to know more about me?
+						</h3>
+						<Link>
+							<button className="btn btn-base">Get to know me.</button>
+						</Link>
 					</header>
 
 					<img
@@ -53,14 +59,14 @@ const Home = () => {
 
 				<section className="flex flex-col py-48 sm:flex-row  parent justify-evenly gap-2 bg-secondary  p-5 items-center">
 					<img
-						className="mask w-2/4 rounded-xl w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+						className="mask rounded-xl w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
 						src="/notebook.jpg"
 						alt="notebook on a table with a pen resting on the page"
 					/>
 					<header className="flex flex-col mask gap-3 max-w-xl prose">
 						<h2 className="text-secondary-content ">Work I've Done.</h2>
-						<h3 className="">
-							I have lots of cool projects; let me show you them.
+						<h3 className="text-secondary-content">
+							I have many cool projects; let me show you them.
 						</h3>
 						<Link to="/projects">
 							<button className="btn btn-primary w-2/5">See My Work</button>
@@ -75,7 +81,7 @@ const Home = () => {
 					<header className="flex flex-col mask gap-3 max-w-xl prose">
 						<h2 className="text-success-content ">Contact Me.</h2>
 						<h3 className="text-success-content ">
-							If you have a cool idea, a position I may be interested in, or
+							If you have an awesome idea, a position I may be interested in, or
 							just want to say hi. Send me a message!
 						</h3>
 						<button className="btn btn-secondary w-2/6">Send a message</button>
