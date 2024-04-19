de37aec317b180f5fb59294f4591e5c2640b69a8
cmattless <connor.mattless@ucd.ie>
2024-04-19 22:16:21 +0100

    form validation

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,49 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { redirect } from "react-router-dom";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
 	const form = useRef();
+	const [message, setMessage] = useState("");
+	const [formErrors, setFormErrors] = useState({});
+
+	const validateForm = () => {
+		let errors = {};
+		let formIsValid = true;
+
+		const name = form.current["name"].value;
+		if (!name) {
+			formIsValid = false;
+			errors["name"] = "Name is required.";
+		}
+
+		const email = form.current["email"].value;
+		if (!email) {
+			formIsValid = false;
+			errors["email"] = "Email is required.";
+		} else if (!/\S+@\S+\.\S+/.test(email)) {
+			formIsValid = false;
+			errors["email"] = "Email is invalid.";
+		}
+
+		
+
+		const message = form.current["message"].value;
+		if (!message) {
+			formIsValid = false;
+			errors["message"] = "Message is required.";
+		}
+
+		setFormErrors(errors);
+		return formIsValid;
+	};
 
 	const sendEmail = (e) => {
 		e.preventDefault();
+
+		if (!validateForm()) {
+			return;
+		}
 
 		emailjs
 			.sendForm("service_fgvta1j", "template_gcoui2i", form.current, {
@@ -13,13 +51,22 @@ const Contact = () => {
 			})
 			.then(
 				() => {
-					console.log("SUCCESS!");
+					setMessage("Email sent successfully!");
+
+					form.current.reset();
+					setTimeout(() => {
+						setMessage("");
+					}, 5000);
 				},
 				(error) => {
-					console.log("FAILED...", error.text);
+					let errors = {};
+					form.current.reset();
+					errors["network"] = error.text;
+					setFormErrors(errors);
 				}
 			);
 	};
+
 	return (
 		<>
 			<main className="container max-w-7xl mx-auto my-5 prose ">
@@ -49,6 +96,7 @@ const Contact = () => {
 									aria-label="Enter email address"
 									aria-required="true"
 								/>
+
 								<textarea
 									name="message"
 									className="textarea  mask textarea-bordered form-item w-full "
@@ -56,6 +104,7 @@ const Contact = () => {
 									aria-label="Message"
 									aria-required="true"
 								></textarea>
+
 								<button
 									type="submit"
 									value="Send"
@@ -63,6 +112,33 @@ const Contact = () => {
 								>
 									Submit
 								</button>
+								{message && (
+									<div className="toast toast-right">
+										<div className="alert alert-success">
+											<span>{message}</span>
+										</div>
+									</div>
+								)}
+								{console.log(Object.values(formErrors))}
+
+								{Object.values(formErrors)?.map((error, i) => (
+										<div key={i} className="alert alert-error">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												className="stroke-primary-content shrink-0 w-6 h-6 "
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													strokeWidth="2"
+													d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+												></path>
+											</svg>
+											<span>{error}</span>
+										</div>
+								))}
 							</form>
 						</div>
 
