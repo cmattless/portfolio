3c125b316329808527e50219f5977f7f8ff53b48
Connor Mattless <connor.mattless@ucd.ie>
2026-04-17 09:16:26 +0100

    fix: update firebase-deploy workflow to use legacy peer dependencies and clean up App component - Added --legacy-peer-deps flag to npm ci in firebase-deploy.yml - Removed unnecessary import of App.css in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import PageNotFound from "./Pages/PageNotFound";
 
-
-import "./App.css";
-
 function App() {
 	return (
 		<>
